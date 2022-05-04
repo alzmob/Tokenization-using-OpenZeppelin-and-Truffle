1be566c61be211f5de70e861093e44ebe456eebb
alzmob <yunayamda3@gmail.com>
2022-05-04 13:53:23 +0900

    react and web3 combined

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,10 +1,10 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.6.1;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+contract MyToken is ERC20, ERC20Detailed {
+    constructor(uint256 initialSupply) ERC20Detailed("StarDucks Cappucino Token", "CAPPU", 0) public {
         _mint(msg.sender, initialSupply);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
-var myToken = artifacts.require("MyToken.sol");
+var MyToken = artifacts.require("MyToken.sol");
+var MyTokenSale = artifacts.require("MyTokenSale");
+var MyKycContract = artifacts.require("KycContract");
+require("dotenv").config({path: "../.env"});
 
 module.exports = async function(deployer) {
-  await deployer.deploy(myToken, 1000000);
+    let addr = await web3.eth.getAccounts();
+    await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
+    await deployer.deploy(MyKycContract);
+    await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address, MyKycContract.address);
+    let instance = await MyToken.deployed();
+    await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS);
+
 }
